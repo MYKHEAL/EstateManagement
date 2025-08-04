52bfeb279dbd2cf33ec6512fbf9264a1576c4e7b
MYKHEAL <ijidolamichaelidowu@gmail.com>
2025-08-04 13:44:29 +0100

    email verification

--- controller/registerAgent.js
+++ controller/registerAgent.js
@@ -1,8 +1,10 @@
 import Agent from '../models/agent.js';
 import bcrypt from 'bcryptjs';
 import cloudinary from '../utils/cloudinary.js';
+import crypto from 'crypto'
+import sendEmail from '../utils/sendEmail.js'
 
-export const registerAgent = assync (req, res) => {
+export const registerAgent = async (req, res) => {
     try{
         const{fullName, email, phone, password} = req.body;
         if(!fullName || !email || !phone || !password || !req.file){
@@ -11,8 +13,20 @@ export const registerAgent = assync (req, res) => {
 
     const existingAgent = await Agent.findOne({email});
     if(existingAgent){
-        return res.status(400).json({message: 'Agent with this email already exists'});
+        return res.status(409).json({message: 'Agent with this email already exists'});
     }
+
+    const tokken = crypto.randomBytes(32).toString('hex');
+    const verificationUrl = `${process.env.BASE_URL}/api/agents/verify-email/${token}`;
+    
+
+    await sendEmail(email, 'Verify Your Email', `
+        <h1>Welcome to Real Estate App</h1>
+        <h2>Email Verification</h2>
+        <p>Click the button or link below to verify your email:</p>
+        <a href="${verificationUrl}">verify Email</a>`);
+
+
 
     const result = await cloudinary.uploader(req.file.path,{
         folder: 'nin_docs'
@@ -24,7 +38,8 @@ export const registerAgent = assync (req, res) => {
         email,
         phone,
         password: await bcrypt.hash(password, 10),
-        ninDocumentUrl: result.secure_url
+        ninDocumentUrl: result.secure_url,
+        emailVerificationToken: token
     })
 
     await agent.save();
