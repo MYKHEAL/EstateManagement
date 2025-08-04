8b202a5572c4ed49152432155c94a19315159b40
MYKHEAL <ijidolamichaelidowu@gmail.com>
2025-08-04 18:57:38 +0100

    added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import emailRoute from './routes/emailRoute.js';
 import agentRoutes from './routes/agentRoutes.js';
 
 dotenv.config();
@@ -10,6 +11,7 @@ app.use(express.json());
 
 // Route
 app.use('/api/agents', agentRoutes);
+app.use('/api', emailRoute);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
